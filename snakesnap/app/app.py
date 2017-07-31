00aed3e1180654f0c2f3ec82b0e3d0a4301d04af
Nick George <nicholas.george32@gmail.com>
2017-07-30 21:50:46 -0600

    form submission not throwing errors, need to handle responses now

--- snakesnap/app/app.py
+++ snakesnap/app/app.py
@@ -3,18 +3,12 @@ import json
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask import flash
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/showSignin')
-def showSignin():
-    return render_template('signin.html')
 
 @app.route('/snakesnap')
 def snakesnap():
@@ -23,13 +17,10 @@ def snakesnap():
 @app.route('/signIn', methods=['GET','POST'])
 def signIn():
     if request.method == 'POST':
-        _name = request.form['inputName']
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
-        flash(_name)
-    if _name and _email and _password:
-        return render_template('home.html')
-    else:
-        return render_template('signin_error.html')
+        username = request.form['name']
+        useremail = request.form['email']
+        userpassword = request.form['password']
+    return render_template("signin.html")
+
 if __name__ == '__main__':
     app.run(debug = True)
