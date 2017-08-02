766879fbadb1330a4ef652b5b2be893d58bfdd78
Nick George <nicholas.george32@gmail.com>
2017-08-01 23:32:09 -0600

    login working but unsafe (json login unhashed) page login checks working

--- snakesnap/app/app.py
+++ snakesnap/app/app.py
@@ -2,8 +2,11 @@
 import json
 from flask import Flask
 from flask import render_template
+import os
 from flask import request
-
+from flask import redirect
+from flask import url_for
+from flask import flash
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,15 +15,25 @@ def home():
 
 @app.route('/snakesnap')
 def snakesnap():
-    return render_template('snakesnap.html')
+    logon = None
+    if os.path.exists('login.json'):
+        with open('login.json', 'r') as login:
+            logon = json.load(login)
+    return render_template('snakesnap.html', logon=logon)
 
 @app.route('/signIn', methods=['GET','POST'])
 def signIn():
     if request.method == 'POST':
+        login_keys = request.form
         username = request.form['name']
-        useremail = request.form['email']
-        userpassword = request.form['password']
-    return render_template("signin.html")
+        flash('Welcome '+username+' you are logged in')
+        with open('login.json', 'w') as logon:
+            json.dump(login_keys, logon)
+        return redirect(url_for('snakesnap'))
+    return render_template('signin.html')
+
+#TODO add logout button to delete login json
 
 if __name__ == '__main__':
+    app.secret_key = 'nick'
     app.run(debug = True)
