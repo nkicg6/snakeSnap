b77c93b579cc982070069b81c22fd66dc92752de
Nick George <nicholas.george32@gmail.com>
2017-08-03 06:29:31 -0600

    start re-organizing password stuff with session cookies. Safer than a json

--- snakesnap/app/app.py
+++ snakesnap/app/app.py
@@ -7,22 +7,31 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from flask import flash
+from flask import session
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
+    """home page displayed on startup"""
+    if 'name' and 'password' in session:
+        return render_template('index.html', name=session['name'])
     return render_template('index.html')
+
 
 @app.route('/snakesnap')
 def snakesnap():
+    """main user functions"""
     logon = None
     if os.path.exists('login.json'):
         with open('login.json', 'r') as login:
             logon = json.load(login)
     return render_template('snakesnap.html', logon=logon)
 
-@app.route('/signIn', methods=['GET','POST'])
+
+@app.route('/signIn', methods=['GET', 'POST'])
 def signIn():
+    """ Main signin page"""
     if request.method == 'POST':
         login_keys = request.form
         username = request.form['name']
@@ -32,8 +41,18 @@ def signIn():
         return redirect(url_for('snakesnap'))
     return render_template('signin.html')
 
-#TODO add logout button to delete login json
+
+@app.route('/logout', method=['POST'])
+def logout():
+    """ make logout function"""
+    pass
+
+# Fix login and logout stuff. get that figured out as session cookies.
+# use this https://pythonspot.com/en/login-authentication-with-flask/
+# and http://flask.pocoo.org/docs/0.12/quickstart/
+# TODO reorganize into header/ footer, and name index page index.
+
 
 if __name__ == '__main__':
     app.secret_key = 'nick'
-    app.run(debug = True)
+    app.run(debug=True)
